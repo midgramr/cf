afee398b82666ef682af7e19da716ae2064b7e8c
Kevin Zhang <kevzhang2022@gmail.com>
2025-11-25 11:33:41 -0600

    Fix logic bug

--- 2164c.py
+++ 2164c.py
@@ -1,5 +1,5 @@
 import sys
-import heapq
+from heapq import heappop, heappush, heapify
 
 input = lambda: sys.stdin.readline().rstrip()
 II = lambda: int(input())
@@ -7,41 +7,47 @@ LII = lambda: list(map(int, input().split()))
 
 def solve():
     _, a, b, c = (LII() for _ in range(4))
-    a.sort()
-    monsters = list(sorted(zip(b, c), key=lambda x: (x[0], -x[1])))
+    heapify(a)
+    monsters = list(sorted(zip(b, c)))
     swords_left, monsters_left, res = [], [], 0
     j = 0
 
-    for x in a:
-        sword = x
-        while j < len(monsters) and sword >= monsters[j][0]:
+    while a:
+        x = heappop(a)
+        add_to_left_heap = True
+        while j < len(monsters) and x >= monsters[j][0]:
             bi, ci = monsters[j]
-            sword = max(sword, ci)
+            j += 1
             if ci > 0:
                 # If we can keep our sword or get a better one, just kill the monster
                 res += 1
+                if ci > x:
+                    # We found a better sword, add back onto our swords heap
+                    heappush(a, ci)
+                    add_to_left_heap = False
+                    break
             else:
                 # If we lose our sword by killing the monster, save it for later
-                heapq.heappush(monsters_left, -bi)
-            j += 1
-        # Sword has one use left; push onto max-heap
-        heapq.heappush(swords_left, -sword)
+                heappush(monsters_left, -bi)
+        if add_to_left_heap:
+            # Sword has one use left; push onto max-heap
+            heappush(swords_left, -x)
 
     while monsters_left and swords_left:
         monster = -monsters_left[0]
-        sword = -swords_left[0]
-        if sword >= monster:
-            heapq.heappop(swords_left)
-            heapq.heappop(monsters_left)
+        x = -swords_left[0]
+        if x >= monster:
+            heappop(swords_left)
+            heappop(monsters_left)
             res += 1
         else:
-            heapq.heappop(monsters_left)
+            heappop(monsters_left)
 
     return res
 
-output = []
-T = II()
-for _ in range(T):
-    ans = solve()
-    output.append(ans)
-print(*output, sep='\n')
+def main():
+    T = II()
+    for _ in range(T):
+        ans = solve()
+        print(ans)
+main()
